146e6299ae202c49e0ebcf0cccd1a816ccc65bdd
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-03-07 10:50:39 +0000

    jQuery namespace

--- jQuery.fastClick.js
+++ jQuery.fastClick.js
@@ -1,6 +1,8 @@
 // http://code.google.com/mobile/articles/fast_buttons.html
 
-google.ui.FastButton = function(element, handler) {
+(function($) {
+
+$.FastButton = function(element, handler) {
   this.element = element;
   this.handler = handler;
 
@@ -8,7 +10,7 @@ google.ui.FastButton = function(element, handler) {
   element.addEventListener('click', this, false);
 };
 
-google.ui.FastButton.prototype.handleEvent = function(event) {
+$.FastButton.prototype.handleEvent = function(event) {
   switch (event.type) {
     case 'touchstart': this.onTouchStart(event); break;
     case 'touchmove': this.onTouchMove(event); break;
@@ -17,7 +19,7 @@ google.ui.FastButton.prototype.handleEvent = function(event) {
   }
 };
 
-google.ui.FastButton.prototype.onTouchStart = function(event) {
+$.FastButton.prototype.onTouchStart = function(event) {
   event.stopPropagation();
 
   this.element.addEventListener('touchend', this, false);
@@ -27,41 +29,41 @@ google.ui.FastButton.prototype.onTouchStart = function(event) {
   this.startY = event.touches[0].clientY;
 };
 
-google.ui.FastButton.prototype.onTouchMove = function(event) {
+$.FastButton.prototype.onTouchMove = function(event) {
   if (Math.abs(event.touches[0].clientX - this.startX) > 10 ||
       Math.abs(event.touches[0].clientY - this.startY) > 10) {
     this.reset();
   }
 };
 
-google.ui.FastButton.prototype.onClick = function(event) {
+$.FastButton.prototype.onClick = function(event) {
   event.stopPropagation();
   this.reset();
   this.handler(event);
 
   if (event.type == 'touchend') {
-    google.clickbuster.preventGhostClick(this.startX, this.startY);
+    $.clickbuster.preventGhostClick(this.startX, this.startY);
   }
 };
 
-google.ui.FastButton.prototype.reset = function() {
+$.FastButton.prototype.reset = function() {
   this.element.removeEventListener('touchend', this, false);
   document.body.removeEventListener('touchmove', this, false);
 };
 
-google.clickbuster.preventGhostClick = function(x, y) {
-  google.clickbuster.coordinates.push(x, y);
-  window.setTimeout(google.clickbuster.pop, 2500);
+$.clickbuster.preventGhostClick = function(x, y) {
+  $.clickbuster.coordinates.push(x, y);
+  window.setTimeout($.clickbuster.pop, 2500);
 };
 
-google.clickbuster.pop = function() {
-  google.clickbuster.coordinates.splice(0, 2);
+$.clickbuster.pop = function() {
+  $.clickbuster.coordinates.splice(0, 2);
 };
 
-google.clickbuster.onClick = function(event) {
-  for (var i = 0; i < google.clickbuster.coordinates.length; i += 2) {
-    var x = google.clickbuster.coordinates[i];
-    var y = google.clickbuster.coordinates[i + 1];
+$.clickbuster.onClick = function(event) {
+  for (var i = 0; i < $.clickbuster.coordinates.length; i += 2) {
+    var x = $.clickbuster.coordinates[i];
+    var y = $.clickbuster.coordinates[i + 1];
     if (Math.abs(event.clientX - x) < 25 && Math.abs(event.clientY - y) < 25) {
       event.stopPropagation();
       event.preventDefault();
@@ -69,5 +71,7 @@ google.clickbuster.onClick = function(event) {
   }
 };
 
-document.addEventListener('click', google.clickbuster.onClick, true);
-google.clickbuster.coordinates = [];
+document.addEventListener('click', $.clickbuster.onClick, true);
+$.clickbuster.coordinates = [];
+
+})(jQuery);
